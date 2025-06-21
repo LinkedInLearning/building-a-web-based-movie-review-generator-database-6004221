398b7d8e8bdc56adf033280d50bc6a672ce56c02
Joe Casabona <jcasabona@gmail.com>
2025-06-21 14:25:06 +0000

    Sorting and Filter fix

---
+++ 5_3/data/actors.php
@@ -0,0 +1,9 @@
+<?php
+// Sample actor data
+$actors = [
+  ['id' => 1, 'name' => 'Jane Ray'],
+  ['id' => 2, 'name' => 'Tom Voss'],
+  ['id' => 3, 'name' => 'Mira Black'],
+  ['id' => 4, 'name' => 'Leo Chan'],
+  // Add more as needed
+];
